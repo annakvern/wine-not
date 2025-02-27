debe1d0137643b1aa5c2911cd7afa77dae0287dc
Anna <anna.kvernplassen@gmail.com>
2025-02-27 18:22:20 +0100

    move images to work with vercel

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -5,19 +5,19 @@ export default function LandingPage() {
   return (
     <StyledLandingPage>
       <Banner
-        imageUrl="images/taste.png"
+        imageUrl="/images/taste.png"
         headline="Need sparkles!"
         fineprint="Twinkle twinkle little star"
         linkTo="/sparkles"
       />
       <Banner
-        imageUrl="images/fav.png"
+        imageUrl="/images/fav.png"
         headline="Can't decide?"
         fineprint="Let me decide for you..."
         linkTo="/random-drink"
       />
       <Banner
-        imageUrl="images/browse.png"
+        imageUrl="/images/browse.png"
         headline="Browse drinks"
         fineprint="Find your favourite!"
         linkTo="/browse"
